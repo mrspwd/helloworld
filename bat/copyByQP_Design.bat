1697e7f45530e936de765eefe0f2df7f545097ca
mrspwd <343506638@qq.com>
2017-03-14 15:59:33 +0800

    bat copy file

---
+++ bat/copyByQP_Design.bat
@@ -0,0 +1,6 @@
+rem 开发代码复制到更新目录 Exclude.txt 排除文件夹或者文件名-排除文件名包含的字符
+xcopy F:\gameproject\Main\Client\QP_Design\src src /e /h /d /y /r /v /f /k /EXCLUDE:Exclude.txt
+
+rem python manifest_export.py
+pause
+
